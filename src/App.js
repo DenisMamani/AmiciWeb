c49cd873b1c4a3e64098d06cfbd72ece00fc121d
Denis Mamani <mamanidenis51@gmail.com>
2022-09-07 19:16:38 -0300

    Agregado de Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+    <header className="header">
+    <nav className="navbar navbar-expand-md navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="./index.html">AmiciWeb</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header">
+              <div className="logoBlanco">logo</div>
+              <button type="button" className="btn-close text-reset bg-secondary" data-bs-dismiss="offcanvas"
+                aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body lista">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav" href="index.html">Inicio</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav" href="./secciones/contacto.html">Contacto</a>
+                </li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
       </header>
-    </div>
+      </div>
   );
 }
 
