b37f5f9cccd5367d077f3b36d3b0b9e9523af6ca
Denis Mamani <mamanidenis51@gmail.com>
2022-09-30 21:43:07 -0300

    Coleccion ordes agregado

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,8 +2,40 @@ import { useContext } from "react"
 import { cartContext } from "./CartContext"
 import { Link } from "react-router-dom"
 import CartItem from './CartItem';
+import { increment, serverTimestamp } from "firebase/firestore";
+import { doc, setDoc, collection, updateDoc } from "firebase/firestore";
+import { db } from "../utils/FireBase";
 const Cart=()=>{
     const {cart,clearCart,totalProductsPrice}=useContext(cartContext)
+    let test = {cart,clearCart,totalProductsPrice}
+    const createOrder = async()=>{
+      let itemsDB= cart.map(product =>({
+       id: product.item.id,
+       price: product.item.price,
+       title:product.item.title,
+       quantity: product.count
+      }))
+      let order ={
+        buyer:{
+          name:"Denis",
+          email:"mamanidenis51@gmail.com",
+          phone: "1138430377"
+        },
+        date: serverTimestamp(),
+        items: itemsDB,
+        total: totalProductsPrice()
+      }
+      const newOrderRef = doc(collection(db,"orders"))
+      await setDoc(newOrderRef, order);
+      cart.forEach(async (item) => {
+        const itemRef = doc(db, "products", item.item.id);
+      await updateDoc(itemRef, {
+        stock:increment(-item.count)
+      });
+      });
+      test.clearCart()
+      alert('la orden ha sido creada' + newOrderRef.id)
+    }
     return(
         <>
          {cart.length === 0 ?
@@ -16,7 +48,7 @@ const Cart=()=>{
           {cart.map(element => <CartItem key={element.item.id} prod={element} />)}
           <p className="totalPrecio">Total: ${totalProductsPrice()}</p>
           <button className="btnEliminarTodo" onClick={() => clearCart()}>Eliminar Todo</button>
-          <button className="btnConfirmar">Confirmar</button>
+          <button className="btnConfirmar" onClick={createOrder} >Confirmar</button>
         </div>
       }
         </>
